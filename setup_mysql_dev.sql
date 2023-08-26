c10bba26bb7163b08b520f8d7a84ee153679908b
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-08-25 17:54:30 -0700

    tasks

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,21 +1,35 @@
--- Creates a MySQL server
---   Database name hbnb_dev_db
---   User hbnb_dev 
---   password hbnb_dev_pwd
---   Grants all privileges for hbnb_dev
---   Grants privilege for hbnb_dev
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance.
+
+-- Connect to the MySQL server as root (adjust the credentials if needed)
+db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='your_root_password'
+)
+
+-- Create a cursor object to execute queries
+cursor = db.cursor()
 
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_dev_db")
 
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Create the user if it doesn't exist and set the password
+cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'")
 
--- Grant privileges to hbnb_dev
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'")
 
--- Grant privilege
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'")
 
--- Flush privileges
-FLUSH PRIVILEGES;
+-- Flush privileges to apply the changes
+cursor.execute("FLUSH PRIVILEGES")
+
+-- Close the cursor and database connection
+cursor.close()
+db.close()
