e421c89d7d58016e9b216363a7d6bbf44d2738db
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-09-14 14:41:26 -0700

    tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ from os.path import exists
 env.hosts = ["54.198.60.71", "100.26.177.201"]
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
 
@@ -25,12 +26,15 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}/'.format(name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, name))
         run('rm /tmp/{}'.format(filename))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name, name))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'.format(name, name))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(name))
         return True
     except Exception as e:
         return False
