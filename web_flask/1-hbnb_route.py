ca648873f6ef309e5ff82dedce7dc38f0817dacd
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-09-27 17:48:52 -0400

    task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -23,4 +23,4 @@ def school():
 if __name__ == "__main__":
     # Start the flask development server
     # Listen on all available network interfces (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5000)
