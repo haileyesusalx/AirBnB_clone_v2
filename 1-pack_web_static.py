d64a67d55dd4ab444eb66a6f6419da2c4aa10794
haileyesus kefyalew <117822035+haileyesusalx@users.noreply.github.com>
2023-09-30 15:30:18 -0400

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static
-folder of your AirBnB Clone repo.
-"""
-
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
-import os
+from fabric.api import local
 
 
 def do_pack():
-    """
-    Create a compressed archive from the web_static folder.
-    Returns:
-        (str): Path to the created archive, or None if archiving fails.
-    """
-    try:
-        # Create the versions folder if it doesn't exist
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-
-        # Generate archive filename using the current date and time
-        now = datetime.now()
-        archive_name = "web_static_{}.tgz".format(
-            now.strftime("%Y%m%d%H%M%S"))
-
-        # Compress the web_static folder into the archive
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-
-        # Return the path to the created archive
-        return "versions/{}".format(archive_name)
-    except Exception:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
